91d56386bfcfbc4ffa9c54d149fb9376f4019d4a
Christian Spinillo <christian.spinillo@gmail.com>
2013-08-28 16:30:40 -0400

    Updated show view to include images.  Updated error messages and validations for Image

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130828163522) do
+ActiveRecord::Schema.define(:version => 20130828195745) do
 
   create_table "pins", :force => true do |t|
     t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.integer  "user_id"
   end
 
